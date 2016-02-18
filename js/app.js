93dff0e394946d5e91e1ab0cbce7708c43eed6ea
antanikmitra <antanikmitra@hotmail.com>
2016-02-18 12:52:37 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(".button-collapse").sideNav();
+
+
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+
+$(document).ready(function () {
+    $('.slider').slider({ full_width: true });
+});
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
+
+
+
+$(document).ready(function () {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
+         
